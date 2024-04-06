1986dd4dd0b633a66f259698b368e7f4bb9d4c42
23winter <abhijeetyadav9489@gmail.com>
2024-04-06 23:51:15 +0530

    moviex

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          "Welcome to our cinema haven! Explore the latest in film magic with
+          our curated collection of trending movies and discover what's
+          captivating audiences worldwide." pariatur.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
+// import React from "react";
+
+// const Footer = () => {
+//   return <div>Footer</div>;
+// };
+
+// export default Footer;
---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.scss";
+
+const PageNotFound = () => {
+  return <div></div>;
+};
+
+export default PageNotFound;
